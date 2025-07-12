0f2a2d0439b5c15002775bc611e213e4ed7ee33c
Arnab0089 <arnabkundu756@gmail.com>
2025-07-12 14:44:08 +0530

    feat: add server entry point and update package.json to use api/index.js

---
+++ Server/api/index.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import cookieParser from 'cookie-parser';
+import connectDB from '../config/mongodb.js';
+import bodyParser from 'body-parser';
+import router from '../Routes/authRouter.js';
+import appRouter from '../Path/appCor.js';
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+app.use(bodyParser.json());
+
+app.use(express.json());
+
+connectDB();
+
+app.use(cors());
+
+app.use('/auth', router);
+app.use('/apps', appRouter);
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the Winget Server');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
